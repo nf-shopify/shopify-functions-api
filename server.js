993d656e72526ed64d7bc900253dc5c1710da13c
Neilson Flemming <Neilson.Flemming@Shopify.com>
2024-08-26 20:58:48 -0400

    added catch all route

--- server.js
+++ server.js
@@ -15,15 +15,21 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(logger("dev"));
 // needed for POST/PUT request
-app.use(express.json())
+app.use(express.json());
 
 /*----- Routes ------*/
 app.get("/", function (req, res) {
-    res.json({ serverStatus: "It's alive" });
-  });
+  res.json({ serverStatus: "It's alive" });
+});
 
 app.use("/functions", functionsRouter);
 app.use("/cartvalidation", cartValidationRouter);
+
+
+// catch all route
+app.use("/*", (req, res) => {
+  res.status(404).json({ error: "Endpoint Not Found" });
+});
 
 /*----- Listener ------*/
 app.listen(PORT, function () {
